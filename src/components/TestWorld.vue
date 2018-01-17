223e59cd85deb4e900e3ea4aba4bf5c9fff36d9c
shushu <907896638@qq.com>
2018-01-17 20:23:29 +0800

    A 测试router

---
+++ src/components/TestWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <a>{{ msg }}</a>
+</template>
+
+<script>
+export default {
+  name: 'TestWorld',
+  data () {
+    // eslint-disable-next-line
+    console.log(this, this.$router, this.$route);
+    return {
+      msg: this.$route.fullPath
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import TestWorld from '@/components/TestWorld'
 
 Vue.use(Router)
 
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/foo',
+      name: 'TestWord',
+      component: TestWorld
+    },
+    {
+      path: '/fcc',
+      name: 'TestWord2',
+      component: TestWorld
     }
   ]
 })

48e33e22e193fd63cefb135c4e21ff5ec7cf385d
shushu <907896638@qq.com>
2018-01-22 00:07:27 +0800

    U app.vue添加header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header class="header">
+      <nav class="header-nav">
+        <a href="/"><img class="logo" src="./assets/logo-48.png" alt="logo"></a>
+        <router-link to="/top">Top</router-link>
+        <router-link to="/new">New</router-link>
+        <router-link to="/show">Show</router-link>
+        <router-link to="/ask">Ask</router-link>
+        <router-link to="/jobs">Jobs</router-link>
+        <a class="user" href="https://github.com/miraclezys">Built with Miraclezys</a>
+      </nav>
+    </header>
     <router-view/>
   </div>
 </template>
@@ -11,13 +21,45 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.header {
+  width: 100%;
+  height: 55px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  background-color: #f60;
+
+  .header-nav {
+    margin: 0 auto;
+    padding: 15px 0;
+    max-width: 800px;
+
+    a {
+      display: inline-block;
+      margin-right: 1.8em;
+      line-height: 24px;
+      color: #fff;
+      opacity: 0.8;
+      font-weight: 300;
+      text-decoration: none;
+      transition: all .3s;
+    }
+
+    .logo {
+      height: 24px;
+      vertical-align: middle;
+    }
+
+    .user {
+      float: right;
+      opacity: 1;
+    }
+
+    .router-link-active {
+      opacity: 1;
+    }
+  }
 }
 </style>
---
+++ src/views/ItemList.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="news-view">
+
+  </div>
+</template>
